20560e48f3221ae1c953ed220a018ce2e9290cc9
amanrajgithub <76594476+amanrajgithub@users.noreply.github.com>
2026-06-21 22:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from dashboard import show_dashboard
-from summary_card_for_dashboard import show_summary
 
 
 st.set_page_config(page_title="Legal Case Management", layout="wide")
