b09909bc26e4558c3778c881ca45a691b4230853
amanrajgithub <76594476+amanrajgithub@users.noreply.github.com>
2026-06-21 19:55:16 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from authlib.integrations.requests_client import OAuth2Session
+
 def google_login():
     client_id = st.secrets["oauth"]["client_id"]
     client_secret = st.secrets["oauth"]["client_secret"]
@@ -8,11 +9,13 @@ def google_login():
     authorization_endpoint = "https://accounts.google.com/o/oauth2/auth"
     userinfo_endpoint = "https://openidconnect.googleapis.com/v1/userinfo"
 
+    # If no token yet
     if "token" not in st.session_state:
         oauth = OAuth2Session(client_id, client_secret, redirect_uri=redirect_uri,
                               scope="openid email profile")
         if st.button("🔑 Login with Google"):
             authorization_url, state = oauth.create_authorization_url(authorization_endpoint)
+            st.markdown(f'<meta http-equiv="refresh" content="0; url={authorization_url}">', unsafe_allow_html=True)
             return authorization_url, None
         return None, None
     else:
